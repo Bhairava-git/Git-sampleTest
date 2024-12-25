7a1bc1df619225dbb7ed42f8d944b96027b2a476
Bhairava-git <thrinadh2497@gmail.com>
2024-12-25 21:42:00 +0530

    Add files via upload

---
+++ GitTest.java
@@ -0,0 +1,16 @@
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        if (x == 10) {
+            System.out.println("Git is working fine");
+
+        } else {
+            System.out.println("Test Failed");
+
+        }
+
+    }
+}
